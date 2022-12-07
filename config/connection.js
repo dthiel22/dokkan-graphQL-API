7da068fbe100a8873d5cb24016ba68d3384dd20a
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-07 10:37:10 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI, 
+  'mongodb+srv://dthiel22:password111@cluster0.ksogziv.mongodb.net/?retryWrites=true&w=majority', 
   // || 'mongodb://127.0.0.1:27017/mern-set-up',
   {
     useNewUrlParser: true,
