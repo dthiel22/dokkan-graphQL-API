2511135a1d5fd94a576923d9cffaa9214c9688bf
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-07 10:41:35 -0800

    changes to connection file, keeping hack way but trying to get .env file to register. dotenv downloaded into package

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb+srv://dthiel22:password111@cluster0.ksogziv.mongodb.net/?retryWrites=true&w=majority', 
-  // || 'mongodb://127.0.0.1:27017/mern-set-up',
+  // 'mongodb+srv://dthiel22:password111@cluster0.ksogziv.mongodb.net/?retryWrites=true&w=majority', 
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-set-up',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
