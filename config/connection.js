4ef49a635ba18b75e45c8e2ac436e6e750876b44
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-07 10:31:21 -0800

    comment mongod local server out

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-set-up',
+  process.env.MONGODB_URI, 
+  // || 'mongodb://127.0.0.1:27017/mern-set-up',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
